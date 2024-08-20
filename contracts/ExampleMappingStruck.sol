2571c7225c372f67def0b17f2cef46c9532fec2b
kaitodecode <kaitodecode@gmail.com>
2024-08-20 14:09:05 +0700

    feat: Mapping struc

--- contracts/ExampleMappingStruck.sol
+++ contracts/ExampleMappingStruck.sol
@@ -2,5 +2,38 @@
 pragma solidity 0.8.14;
 
 contract ExampleMappingStruck {
-    
+    struct Transaction{
+        uint amount;
+        uint timestamp;
+    }
+
+    struct Balance{
+        uint totalBalance;
+        uint numDeposits;
+        mapping(uint => Transaction) deposits;
+        uint numWithDrawals;
+        mapping(uint => Transaction) withdrawals; 
+    }
+
+    mapping(address => Balance) public balances;
+
+    function getDepositNum(address _from, uint _id) public view returns (Transaction memory){
+        return balances[_from].deposits[_id];
+    }
+
+    function depositMoney() public payable  {
+        balances[msg.sender].totalBalance += msg.value;
+        Transaction memory deposit = Transaction(msg.value,block.timestamp);
+        balances[msg.sender].deposits[balances[msg.sender].numDeposits] = deposit;
+        balances[msg.sender].numDeposits++;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        balances[msg.sender].totalBalance -= _amount;
+        Transaction memory withdrawal = Transaction(_amount,block.timestamp);
+        balances[msg.sender].withdrawals[balances[msg.sender].numWithDrawals] = withdrawal;
+        balances[msg.sender].numWithDrawals++;
+        _to.transfer(_amount);
+        
+    }
 }
